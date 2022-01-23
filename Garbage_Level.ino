80bceaaee74e6f6ba846e842277949183e202a28
NimashaKWeerasinghe <91545792+NimashaKWeerasinghe@users.noreply.github.com>
2022-01-23 22:54:14 +0530

    Added LED bulb
    Switch on the LED bulb when the garbage level reaches 85%.

--- Garbage_Level.ino
+++ Garbage_Level.ino
@@ -1,18 +1,22 @@
 const int trigPin = 5;
 const int echoPin = 18;
+const int ledPin = 5;
 
 //define sound speed in cm/uS
 #define SOUND_SPEED 0.034
 #define CM_TO_INCH 0.393701
+#define bin_depth 100
 
 long duration;
 float distanceCm;
 float distanceInch;
+float bin_percentage;
 
 void setup() {
   Serial.begin(115200); // Starts the serial communication
   pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
   pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+  pinMode (ledPin, OUTPUT);
 }
 
 void loop() {
@@ -34,10 +38,24 @@ void loop() {
   distanceInch = distanceCm * CM_TO_INCH;
   
   // Prints the distance in the Serial Monitor
-  Serial.print("Distance (cm): ");
+  Serial.print("Distance to garbage (cm): ");
   Serial.println(distanceCm);
   //Serial.print("Distance (inch): ");
   //Serial.println(distanceInch);
+
+  bin_percentage = ((bin_depth-distanceCm) / bin_depth) * 100;
+
+  Serial.print("Garbage fill percentage: ");
+  Serial.println(bin_percentage);
+
+  if (bin_percentage >= 85)
+  {
+    digitalWrite (ledPin, HIGH);
+  }
+  else
+  {
+    digitalWrite (ledPin, LOW);
+  }
   
   delay(1000);
 }
