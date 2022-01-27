f8d65b7577517ee1c64b2a772789c52178861a1f
Dewmini <91756086+DewminiSeneviratne@users.noreply.github.com>
2022-01-27 21:48:59 +0530

    Added WiFi connection

--- swdss_device_1.ino
+++ swdss_device_1.ino
@@ -1,25 +1,49 @@
+//WiFi
+#define WIFI_SSID "Farmaan A10s"
+#define WIFI_PASSWORD "ptgp4252"
+//define sound speed in cm/uS
+#define SOUND_SPEED 0.034
+#define bin_depth 100
+
+#include <WiFi.h>
+
 const int trigPin = 5;
 const int echoPin = 18;
 const int ledPin = 5;
 
-//define sound speed in cm/uS
-#define SOUND_SPEED 0.034
-#define CM_TO_INCH 0.393701
-#define bin_depth 100
-
 long duration;
 float distanceCm;
-float distanceInch;
 float bin_percentage;
 
 void setup() {
   Serial.begin(115200); // Starts the serial communication
+  Wifi_Init();
   pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
   pinMode(echoPin, INPUT); // Sets the echoPin as an Input
-  pinMode (ledPin, OUTPUT);
+  pinMode(ledPin, OUTPUT);
+  pinMode(buzzer, OUTPUT);
+}
+
+void Wifi_Init(){
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Connecting to Wi-Fi");
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print(".");
+    delay(1000);
+  }
+  Serial.println();
+  Serial.print("Connected with IP: ");
+  Serial.println(WiFi.localIP());
+  Serial.println();
 }
 
 void loop() {
+  garbage_level();
+}
+
+void garbage_level(){
   // Clears the trigPin
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
@@ -34,15 +58,10 @@ void loop() {
   // Calculate the distance
   distanceCm = duration * SOUND_SPEED/2;
   
-  // Convert to inches
-  distanceInch = distanceCm * CM_TO_INCH;
-  
   // Prints the distance in the Serial Monitor
   Serial.print("Distance to garbage (cm): ");
   Serial.println(distanceCm);
-  //Serial.print("Distance (inch): ");
-  //Serial.println(distanceInch);
-
+  
   bin_percentage = ((bin_depth-distanceCm) / bin_depth) * 100;
 
   Serial.print("Garbage fill percentage: ");
